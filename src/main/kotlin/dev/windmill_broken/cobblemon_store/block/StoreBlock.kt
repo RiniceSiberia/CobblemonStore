1dbbfc9f68199ffa666fa929bb5c21a251fb0182
Hypocisy <1137943609@qq.com>
2025-06-21 23:37:35 +0800

    - Fixed store block model

--- src/main/kotlin/dev/windmill_broken/cobblemon_store/block/StoreBlock.kt
+++ src/main/kotlin/dev/windmill_broken/cobblemon_store/block/StoreBlock.kt
@@ -3,23 +3,27 @@ package dev.windmill_broken.cobblemon_store.block
 import com.cobblemon.mod.common.util.writeString
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
-import dev.windmill_broken.cobblemon_store.screen.StoreSelectionScreen
 import dev.windmill_broken.cobblemon_store.menu.StoreMenuProvider
+import dev.windmill_broken.cobblemon_store.screen.StoreSelectionScreen
 import net.minecraft.client.Minecraft
 import net.minecraft.core.BlockPos
 import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.BaseEntityBlock
-import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.RenderShape
-import net.minecraft.world.level.block.SoundType
+import net.minecraft.world.level.block.*
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BooleanProperty
+import net.minecraft.world.level.block.state.properties.DirectionProperty
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument
 import net.minecraft.world.level.material.MapColor
 import net.minecraft.world.level.material.PushReaction
 import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class StoreBlock(
     properties: Properties = Properties.of()
@@ -33,6 +37,18 @@ class StoreBlock(
         .isValidSpawn(Blocks::never)
         .sound(SoundType.EMPTY).ignitedByLava()
 ) {
+    private val AABB: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 14.0, 15.0)
+
+    companion object {
+        val FACING: DirectionProperty = BlockStateProperties.HORIZONTAL_FACING
+        val WATERLOGGED: BooleanProperty? = BlockStateProperties.WATERLOGGED
+    }
+
+    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        super.createBlockStateDefinition(builder)
+        builder.add(FACING, WATERLOGGED)
+    }
+
     private val CODEC: MapCodec<StoreBlock> =
         RecordCodecBuilder.mapCodec { instance: RecordCodecBuilder.Instance<StoreBlock> ->
             instance.group(
@@ -83,6 +99,14 @@ class StoreBlock(
         return InteractionResult.sidedSuccess(level.isClientSide)
     }
 
+    override fun useShapeForLightOcclusion(state: BlockState): Boolean {
+        return true
+    }
+
+    override fun getOcclusionShape(state: BlockState, level: BlockGetter, pos: BlockPos): VoxelShape {
+        return AABB // 即满立方体
+    }
+
     override fun codec(): MapCodec<out BaseEntityBlock?> {
         return CODEC
     }
@@ -92,6 +116,15 @@ class StoreBlock(
         p1: BlockState
     ): StoreBlockEntity {
         return StoreBlockEntity(p0, p1)
+    }
+
+    override fun getShape(
+        state: BlockState,
+        level: BlockGetter,
+        pos: BlockPos,
+        context: CollisionContext
+    ): VoxelShape {
+        return AABB
     }
 
     override fun getRenderShape(state: BlockState): RenderShape {
